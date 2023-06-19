b8eb9f6683cbda300d0b0b8b572d61b575e54211
BiBi-Techbro <blessingodinaka24@gmail.com>
2023-06-19 15:39:09 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-* main - Prints the alphabet.
-*
-* Return: Always 0 (Success)
-*/
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -17,4 +17,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,10 @@
 nclude <stdio.h>
 
 /**
-* main - Prints numbers from 0 to 9.
-*
-* Return: Always 0 (Success)
-*/
+ * main - Prints numbers from 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
